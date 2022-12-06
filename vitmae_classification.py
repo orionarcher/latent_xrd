vitmae classification corrected softmax

vitmae_classification.py
@@ -6,7 +6,7 @@ from torch import nn,optim
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = ViTForImageClassification.from_pretrained("./classification_xrd_xrdgaussian/")
+model = ViTForImageClassification.from_pretrained("./classification/")
 model= nn.DataParallel(model)
 model.to(device)
 
@@ -37,7 +37,7 @@ def train_model(num_epochs=100):
             loss.backward()
             optimizer.step()
             if idx % 1000 == 0:
-                torch.save(model.state_dict(), f'/pscratch/sd/h/hasitha/xrd/vitmae_classification_xrd_xrdgaussian/vitmae_classification_xrd_xrdgaussian_epoch_{epoch}_batch_{idx}.pth')
+                torch.save(model.state_dict(), f'/pscratch/sd/h/hasitha/xrd/vitmae_classification/vitmae_classification_epoch_{epoch}_batch_{idx}.pth')
             if idx % 5 == 0:
                 print(f"Finished batch {idx} in epoch {epoch + 1}. Loss: {loss.item():.4f}")
 
@@ -50,7 +50,7 @@ train_model(num_epochs=100)
 model.train(False)
 
 print('Finished Training, saving the model')
-torch.save(model.state_dict(), '/global/homes/h/hasitha/latent_xrd/vitmae_classification_xrd_xrdgaussian.pth')
+torch.save(model.state_dict(), '/global/homes/h/hasitha/latent_xrd/vitmae_classification.pth')
 print('Model saved')
 
 