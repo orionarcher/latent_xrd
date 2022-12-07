0f369bbdfd4e08fbc8688a82836c1bdd6ccad9c8
Hasitha-Wijesuriya <hasithawijesooriya@gmail.com>
2022-12-07 14:45:58 -0800

    vitmae with 80-20 train test case

--- dataloader.py
+++ dataloader.py
@@ -15,8 +15,8 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 
-XRD_DATA_PATH = "/pscratch/sd/h/hasitha/xrd/icsd_data_189476_10000_cry_extinction_space_density_vol.h5"
-# XRD_DATA_PATH = "icsd_data_189476_10000_cry_extinction_space_density_vol.h5"
+# XRD_DATA_PATH = "/pscratch/sd/h/hasitha/xrd/icsd_data_189476_10000_cry_extinction_space_density_vol.h5"
+XRD_DATA_PATH = "icsd_data_189476_10000_cry_extinction_space_density_vol.h5"
 if not path.exists(XRD_DATA_PATH):
     raise FileNotFoundError(
         f'File "{XRD_DATA_PATH}" does not exist! '
@@ -28,7 +28,7 @@ if not path.exists(XRD_DATA_PATH):
 # DataLoader parameters – These are yours to tweak. Feel free to modify them! #
 ###############################################################################
 
-BATCH_SIZE = 100
+BATCH_SIZE = 5
 SHUFFLE = True
 NUM_WORKERS = 0
 
@@ -280,11 +280,11 @@ class SquareXRDDataset_classification(Dataset):
         self.num_samples: int = self.xrd.shape[0]
 
     def __len__(self) -> int:
-        return self.num_samples
+        return int(self.num_samples*0.8)
 
     def __getitem__(self, index: int) -> np.ndarray:
         """Returns an XRD spectra array with shape (10005,)."""
-
+        
         # Last 5 elements in XRD data aren't part of the spectra
         sample = self.xrd[index][:-5]
         classification = self.xrd[index][10000]
@@ -312,3 +312,92 @@ square_xrd_classification_dataloader = DataLoader(
     shuffle=SHUFFLE,
     num_workers=NUM_WORKERS
 )
+
+class test_SquareXRDDataset_classification(Dataset):
+    """Represents an XRD dataset.
+
+    XRD Dataset must be HDF5.
+    file_path: Path to HDF5 (.h5) file.
+    """
+
+    def __init__(self, file_path: str):
+        file = h5py.File(file_path, "r")
+
+        self.xrd: h5py.Dataset = file["data"]
+        self.num_samples: int = self.xrd.shape[0]
+
+    def __len__(self) -> int:
+        return int(self.num_samples*0.2)
+
+    def __getitem__(self, index: int) -> np.ndarray:
+        """Returns an XRD spectra array with shape (10005,)."""
+        index = -index - 1 
+        # Last 5 elements in XRD data aren't part of the spectra
+        sample = self.xrd[index][:-5]
+        classification = self.xrd[index][10000]
+        classification_row = classification * np.ones(100)
+        
+
+        edge_sqrt = 10
+        tile = np.reshape(np.arange(edge_sqrt ** 2), (edge_sqrt,edge_sqrt))
+        a = np.repeat(tile, edge_sqrt, axis=0)
+        b = np.repeat(a, edge_sqrt, axis=1)
+        tiled = np.tile(tile, (edge_sqrt, edge_sqrt))
+        arr = tiled + b * edge_sqrt ** 2
+        sample = sample[arr]
+        sample = np.vstack((sample, classification_row))
+
+        square = sample.reshape(1, 101, 100)
+
+        # Add an axis to the array to fit the Perceiver input
+        return torch.from_numpy(square).float()
+
+test_square_xrd_classification_dataset = test_SquareXRDDataset_classification(XRD_DATA_PATH)
+test_square_xrd_classification_dataloader = DataLoader(
+    test_square_xrd_classification_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=SHUFFLE,
+    num_workers=NUM_WORKERS
+)
+
+
+class SquareBinaryDatasetGaussian(Dataset):
+    """Represents an XRD dataset.
+    XRD Dataset must be HDF5.
+    file_path: Path to HDF5 (.h5) file.
+    """
+
+    def __init__(self, file_path: str):
+        file = h5py.File(file_path, "r")
+        self.indexes = np.arange(0, 10000)
+
+        self.xrd: h5py.Dataset = file["data"]
+        self.num_samples: int = self.xrd.shape[0]
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def __getitem__(self, index: int) -> np.ndarray:
+        """Returns an XRD spectra array with shape (10005,)."""
+
+        # Last 5 elements in XRD data aren't part of the spectra
+        ones = np.random.choice(self.indexes, size=100)
+        zeros = np.zeros(10000)
+        zeros[ones] = 1
+        
+        blured = scipy.ndimage.gaussian_filter1d(zeros, 4)
+        square = blured.reshape(1, 100, 100)
+
+        # Add an axis to the array to fit the Perceiver input
+        return torch.from_numpy(square).float()
+
+
+square_binary_dataset_gaussian = SquareBinaryDatasetGaussian(XRD_DATA_PATH)
+square_binary_dataloader_gaussian = DataLoader(
+    square_binary_dataset_gaussian,
+    batch_size=BATCH_SIZE,
+    shuffle=SHUFFLE,
+    num_workers=NUM_WORKERS
+)
+
+
--- vitmae_classification.py
+++ vitmae_classification.py
@@ -1,5 +1,5 @@
 from transformers import ViTForImageClassification
-from dataloader import square_xrd_classification_dataloader
+from dataloader import square_xrd_classification_dataloader, test_square_xrd_classification_dataloader
 import torch
 import numpy as np
 from torch import nn,optim
@@ -14,10 +14,11 @@ model.to(device)
 
 def train_model(num_epochs=100):
     cross_entropy = nn.CrossEntropyLoss()
-    soft_max = nn.Softmax(dim = 1)
+    # soft_max = nn.Softmax(dim = 1)
     outputs = []
     optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-5)
     for epoch in range(num_epochs):
+        lst_loss = []
         for idx, data in enumerate(square_xrd_classification_dataloader):
             classification = data[:, :, -1, 0]-1
             classification_gpu = torch.from_numpy(classification.long().numpy()[:,0])
@@ -33,16 +34,30 @@ def train_model(num_epochs=100):
             output = model(data)
             # soft_max_output = soft_max(output.logits)
             loss = cross_entropy(output.logits, classification_gpu)
+            lst_loss.append(loss.item())
             # ===================backward====================
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             if idx % 1000 == 0:
                 torch.save(model.state_dict(), f'/pscratch/sd/h/hasitha/xrd/vitmae_classification/10_masking_80_data/vitmae_classification_10_mask_80_data_epoch_{epoch}_batch_{idx}.pth')
-            if idx % 5 == 0:
+            if idx % 10 == 0:
                 print(f"Finished batch {idx} in epoch {epoch + 1}. Loss: {loss.item():.4f}")
+        # test loop
+        for idx, data in enumerate(test_square_xrd_classification_dataloader):
+            lst_loss_train = []
+            classification = data[:, :, -1, 0]-1
+            classification_gpu = torch.from_numpy(classification.long().numpy()[:,0])
+            data = data[:, :, :-1, :]
+            data = data.to(device)
+            classification_gpu = classification_gpu.to(device)
+            output = model(data)
+            loss_train = cross_entropy(output.logits, classification_gpu)
+            lst_loss_train.append(loss_train.item())
+            
 
-        print('epoch [{}/{}], loss:{:.7f}'.format(epoch + 1, num_epochs, loss.item()))
+        print('epoch [{}/{}], loss:{:.7f}'.format(epoch + 1, num_epochs, sum(lst_loss)/len(lst_loss)))
+        print('train epoch_t [{}/{}], loss:{:.7f}'.format(epoch + 1, num_epochs, sum(lst_loss_train)/len(lst_loss_train)))
         outputs.append((epoch, data, output))
 
 
